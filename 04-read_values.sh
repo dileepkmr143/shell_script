e82aa4366421e64044f81db3001d63d52c43aecb
Bandi <dilipkumar.bandi@dxc.com>
2023-12-14 14:03:12 +0530

    new commit

---
+++ 04-read_values.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter username"
+
+read -s username
+
+echo "Username entered is $username"
+
+echo "please enter password"
+
+read -s password
+
+echo "entered username is $username and password is $password"
