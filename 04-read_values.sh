270ee3e58f5febe8b4f931e1e42f83cc48b6933e
Bandi <dilipkumar.bandi@dxc.com>
2023-12-14 14:09:27 +0530

    new commit

--- 04-read_values.sh
+++ 04-read_values.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo "please enter username"
+R="\e[31m"
+Y="\e[32m"
+N="\e[0m"    
+    echo "please enter username"
 
 read -s username
 
-echo "Username entered is $username"
+echo "Username entered is $R $username $N"
 
 echo "please enter password"
 
 read -s password
 
-echo "entered username is $username and password is $password"
+echo "entered username is $R $username $N and password is $R $password $N"
