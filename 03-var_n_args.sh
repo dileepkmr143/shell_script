3f3a1589050a6fcd3d27f169c65ed069ea357968
Bandi <dilipkumar.bandi@dxc.com>
2023-12-14 14:16:07 +0530

    new commit

--- 03-var_n_args.sh
+++ 03-var_n_args.sh
@@ -4,10 +4,11 @@ username=$1
 password=$2
 RED="\e[31m"
 NONE="\e[0m"
+timestamp=$((date)+H)
 
 echo -e "Username entered is $RED $username $NONE"
 echo -e "Password entered is $password"
 
-echo "total no of args passed are $#"
+echo "total no of args passed are $# and date is $timestamp"
 echo "args passed are $@"
 echo "script name passed $0"
