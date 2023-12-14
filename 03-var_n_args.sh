93af850d1c57cfa04cc1173075bad7836789dd1b
Bandi <dilipkumar.bandi@dxc.com>
2023-12-14 14:14:51 +0530

    new commit

--- 03-var_n_args.sh
+++ 03-var_n_args.sh
@@ -7,3 +7,7 @@ NONE="\e[0m"
 
 echo -e "Username entered is $RED $username $NONE"
 echo -e "Password entered is $password"
+
+echo "total no of args passed are $#"
+echo "args passed are $@"
+echo "script name passed $0"
