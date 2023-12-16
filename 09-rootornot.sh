d6727ff9fa8a5a0610877798a5dc487a91501d56
Bandi <dilipkumar.bandi@dxc.com>
2023-12-16 10:44:00 +0530

    new commit

--- 09-rootornot.sh
+++ 09-rootornot.sh
@@ -7,7 +7,7 @@ echo "check user is sudo or not $new"
 if [ $new -eq 0 ]
 then
     echo "user is sudo user"
-    yum install mysql -y & >>logfile
+    yum install mysql -y &>> logfile
 else
     echo "install mysql with sudo privileges"
     exit 1
