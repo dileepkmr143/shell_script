835a23580ccc206de8ecbd6fda6fadb193fabc47
Bandi <dilipkumar.bandi@dxc.com>
2023-12-16 07:08:48 +0530

    new commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -8,3 +8,9 @@ echo "sum of two numbers is $(($test1+$test2))"
 sum=$(($test1+$test2))
 
 echo "summing two numbers $sum"
+
+echo "total args passed $@"
+
+echo "total no of args passed $#"
+
+echo "exit code of last run command $?"
