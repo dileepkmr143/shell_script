0b81c50e4d27119909ad99ffefe937e62bcbc9e1
Bandi <dilipkumar.bandi@dxc.com>
2023-12-16 09:06:13 +0530

    new commit

--- 08-forloop.sh
+++ 08-forloop.sh
@@ -8,7 +8,7 @@ echo "please enter number2"
 
 read $num2
 
-for (( i=1: i <= $num1: i++ ))
+for (( i=1 : i <= $num1 : i++ ))
 do
-    echo "number to be print $num1"
+    echo "number to be print $i"
 done
