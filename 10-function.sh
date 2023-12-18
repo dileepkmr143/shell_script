8c0b8884f61a9f790af64960fb60cc049e6d02f3
Bandi <dilipkumar.bandi@dxc.com>
2023-12-18 18:54:15 +0530

    new commit

--- 10-function.sh
+++ 10-function.sh
@@ -2,11 +2,16 @@
 
 sud=$(id -u)
 
+logfile=/tmp/$0-$(date +%F-%H-%M-%S).log
+
+echo $logfile
+
 if [ $sud -eq 0 ]
     then
     echo "user has sudo access"
     else
     echo "run installation with sudo access"
+    exit 1
 fi
 
 newfunction(){
@@ -22,4 +27,16 @@ if [ $1 -eq 0 ]
 yum install mysql -y
 
 newfunction $? "installing mysql"
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -eq 0 ]
+    then
+        echo "$package is already installed"
+    else
+        yum install $package -y &>> logfile
+        newfunction $? "$package installation is success"
+    fi
+done
 
